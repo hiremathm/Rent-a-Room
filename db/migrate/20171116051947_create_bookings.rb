a14e7d4614387f406ab1839a6e34acdf43d949b0
shviakumara m <shivasorab@gmail.com>
2018-01-20 16:59:20 +0530

    20-01-2017

--- db/migrate/20171116051947_create_bookings.rb
+++ db/migrate/20171116051947_create_bookings.rb
@@ -1,8 +1,8 @@
 class CreateBookings < ActiveRecord::Migration
   def change
     create_table :bookings do |t|
-      t.date :start_date
-      t.date :end_date
+      t.string :start_date
+      t.string :end_date
       t.integer :price
       t.integer :user_id
       t.integer :room_id
