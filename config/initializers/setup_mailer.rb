d3489d842c250bd72ace53281818ba9cd1d05cc2
shivakumara m <shivasorab@gmail.com>
2018-04-23 17:56:23 +0530

    password

--- config/initializers/setup_mailer.rb
+++ config/initializers/setup_mailer.rb
@@ -5,6 +5,6 @@ ActionMailer::Base.smtp_settings = {
    :authentication => "plain",
    :enable_starttls_auto => true,
    :user_name => "shivasorab@gmail.com",
-   :password => "fullstackdeveloper",
+   :password => "shiva@123",
    :openssl_verify_mode  => 'none'
 }
