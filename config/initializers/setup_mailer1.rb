d23b129857672de230d6da0f7a4691b1ace1bcf5
Shivakumara M <shivasorab@gmail.com>
2019-07-04 22:51:56 +0530

    mailer setup changes

---
+++ config/initializers/setup_mailer1.rb
@@ -0,0 +1,13 @@
+  config_setup = Config.where(config_id: "5001", title: "paytm").last
+  config_array = config_setup.info.map { |k| eval(k)}
+  mailer_setup = config_array.map {|p| p if p[:environment] == "development"}.compact.last 
+  
+ActionMailer::Base.smtp_settings = {
+  :address => mailer_setup[:address],
+  :port => mailer_setup[:port],
+  :domain => mailer_setup[:domain],
+  :user_name =>  mailer_setup[:user_name],
+  :password =>  mailer_setup[:password],
+  :authentication => mailer_setup[:authentication],
+  :enable_starttls_auto => mailer_setup[:enable_starttls_auto]
+}
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190625013413) do
+ActiveRecord::Schema.define(version: 20190702002834) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,14 @@ ActiveRecord::Schema.define(version: 20190625013413) do
     t.string   "slug"
     t.string   "state_code"
     t.string   "city_code"
+  end
+
+  create_table "configs", force: :cascade do |t|
+    t.integer  "config_id"
+    t.string   "title"
+    t.text     "info",       default: [],              array: true
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
   end
 
   create_table "delayed_jobs", force: :cascade do |t|
