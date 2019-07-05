87985296a68e7d3d885a06e1acc3b15ecb46d74f
Shivakumara M <shivasorab@gmail.com>
2019-07-05 20:30:16 +0530

    mailer setup file commented

--- config/initializers/setup_mailer.rb
+++ config/initializers/setup_mailer.rb
@@ -1,14 +1,14 @@
-  config_setup = Config.where(config_id: "5001", title: "paytm").last
-  config_array = config_setup.info.map { |k| eval(k)}
-  env = Rails.env
-  mailer_setup = config_array.map {|p| p if p[:environment] == env}.compact.last 
+#   config_setup = Config.where(config_id: "5001", title: "paytm").last
+#   config_array = config_setup.info.map { |k| eval(k)}
+#   env = Rails.env
+#   mailer_setup = config_array.map {|p| p if p[:environment] == env}.compact.last 
   
-ActionMailer::Base.smtp_settings = {
-  :address => mailer_setup["address"],
-  :port => mailer_setup["port"],
-  :domain => mailer_setup["domain"],
-  :user_name =>  mailer_setup["user_name"],
-  :password =>  mailer_setup["password"],
-  :authentication => mailer_setup["authentication"],
-  :enable_starttls_auto => true
-}
+# ActionMailer::Base.smtp_settings = {
+#   :address => mailer_setup["address"],
+#   :port => mailer_setup["port"],
+#   :domain => mailer_setup["domain"],
+#   :user_name =>  mailer_setup["user_name"],
+#   :password =>  mailer_setup["password"],
+#   :authentication => mailer_setup["authentication"],
+#   :enable_starttls_auto => true
+# }
