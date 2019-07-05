ed686ea8a1e015adcd628834e80689f2b23d424a
Shivakumara M <shivasorab@gmail.com>
2019-07-05 22:22:48 +0530

    table exist active record connection query written inside the setup mailer configuration

--- config/initializers/setup_mailer.rb
+++ config/initializers/setup_mailer.rb
@@ -1,14 +1,22 @@
-config_setup = Config.where(config_id: "5001", title: "paytm").last
-config_array = config_setup.info.map { |k| eval(k)}
-env = Rails.env
-mailer_setup = config_array.map {|p| p if p[:environment] == env}.compact.last 
-  
-ActionMailer::Base.smtp_settings = {
-  :address => mailer_setup["address"],
-  :port => mailer_setup["port"],
-  :domain => mailer_setup["domain"],
-  :user_name =>  mailer_setup["user_name"],
-  :password =>  mailer_setup["password"],
-  :authentication => mailer_setup["authentication"],
-  :enable_starttls_auto => true
-}
+status = ActiveRecord::Base.connection.table_exists? 'config'
+if status
+  # puts "Config Table existed"
+  config_setup = Config.where(config_id: "5001", title: "paytm").last
+  if config_setup.length != 0 && config_setup.length > 0 
+    # puts "Configuration existed"
+    puts "config setup : " + "#{config_setup}"
+    config_array = config_setup["info"].map { |k| eval(k)}
+    env = Rails.env
+    mailer_setup = config_array.map {|p| p if p[:environment] == env}.compact.last 
+      
+    ActionMailer::Base.smtp_settings = {
+      :address => mailer_setup["address"],
+      :port => mailer_setup["port"],
+      :domain => mailer_setup["domain"],
+      :user_name =>  mailer_setup["user_name"],
+      :password =>  mailer_setup["password"],
+      :authentication => mailer_setup["authentication"],
+      :enable_starttls_auto => true
+    }
+  end
+end
