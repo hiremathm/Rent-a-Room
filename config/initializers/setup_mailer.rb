df1234d3f04afaba13bf13176f272223906149a5
Shivakumara m <32825358+shiva-9731@users.noreply.github.com>
2019-08-15 18:07:31 +0530

    Mailer setup added

--- config/initializers/setup_mailer.rb
+++ config/initializers/setup_mailer.rb
@@ -1,1 +1,9 @@
-
+ActionMailer::Base.smtp_settings = {
+:address => Rails.application.secrets[:address],
+:port => Rails.application.secrets[:port],
+:domain => Rails.application.secrets[:domain],
+:user_name =>  Rails.application.secrets[:mail_id],
+:password =>  Rails.application.secrets[:password],
+:authentication => Rails.application.secrets[:authentication],
+:enable_starttls_auto => Rails.application.secrets[:enable_starttls_auto]
+}
