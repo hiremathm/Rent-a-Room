0ecbe533154327b5131ba46807c2ef7444c66848
Shivakumara M <shivasorab@gmail.com>
2020-06-22 09:44:35 +0530

    module conflict solved

--- lib/paytm_helper.rb
+++ lib/paytm_helper.rb
@@ -3,8 +3,4 @@ end
 
 require_dependency "#{Rails.root}/lib/paytm_helper/encryption_new_pg.rb"
 require_dependency "#{Rails.root}/lib/paytm_helper/checksum_tool.rb"
-<<<<<<< HEAD
 require_dependency "#{Rails.root}/lib/paytm_helper/utility.rb"
-=======
-require_dependency "#{Rails.root}/lib/paytm_helper/utility.rb"
->>>>>>> 2b049c397a3e85d7fdd62e5cfcbfbace966a93f3
